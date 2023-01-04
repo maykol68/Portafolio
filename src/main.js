10b622302f18a90a1cb8fa2873fc7122b64af937
Andres1124 <andresdelgadoperez19@gmail.com>
2023-01-04 18:27:57 -0500

    add fontaweson icons in project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import FontAwesomeIcon from '@/utils/fontAwesomeIcons'
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
---
+++ src/utils/fontAwesomeIcons.js
@@ -0,0 +1,11 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faPhone, faUser } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+library.add(faPhone, faUser)
+
+export default FontAwesomeIcon
